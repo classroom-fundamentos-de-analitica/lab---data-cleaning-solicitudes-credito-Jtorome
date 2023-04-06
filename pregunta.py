34414f89d8f8fab9bd4573815d19aad9f5925d5c
Juan Alberto Toro Mejia <juanalbtoro@hotmail.com>
2023-04-06 01:06:28 -0500

    finished

--- pregunta.py
+++ pregunta.py
@@ -13,8 +13,13 @@ def clean_data():
 
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
 
-    #
-    # Inserte su código aquí
-    #
-
+    df['sexo'] = df['sexo'].apply(str.lower)
+    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].apply(lambda x: x.lower() if type(x) == str else x)
+    df['idea_negocio'] = df['idea_negocio'].apply(lambda x: x.lower().replace('_', ' ').replace('-', ' ') if type(x) == str else x)
+    df['barrio'] = df['barrio'].apply(lambda x: x.lower().replace('_', ' ').replace('-', ' ') if type(x) == str else x)
+    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'], format='%d/%m/%Y', errors='coerce').fillna(
+        pd.to_datetime(df['fecha_de_beneficio'], format='%Y/%m/%d', errors='coerce')
+    )
+    df['monto_del_credito'] = df['monto_del_credito'].apply(lambda x: x.replace('$', '').strip().replace(',', '').replace('.00', '') if type(x) == str else x)
+    df['línea_credito'] = df['línea_credito'].apply(lambda x: x.lower().replace('_', ' ').replace('-', ' ') if type(x) == str else x)
     return df
