9d33bc46638e34009a24d86a8a781b17007d4129
Juan Alberto Toro Mejia <juanalbtoro@hotmail.com>
2023-04-06 10:07:08 -0500

    finished

--- pregunta.py
+++ pregunta.py
@@ -11,15 +11,20 @@ import pandas as pd
 
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0, encoding="utf-8")
 
+    df = df.dropna()
     df['sexo'] = df['sexo'].apply(str.lower)
-    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].apply(lambda x: x.lower() if type(x) == str else x)
-    df['idea_negocio'] = df['idea_negocio'].apply(lambda x: x.lower().replace('_', ' ').replace('-', ' ') if type(x) == str else x)
+    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].apply(str.lower)
+    df['idea_negocio'] = df['idea_negocio'].apply(lambda x: x.lower().replace('_', ' ').replace('-', ' ').strip() if type(x) == str else x)
     df['barrio'] = df['barrio'].apply(lambda x: x.lower().replace('_', ' ').replace('-', ' ') if type(x) == str else x)
+    df['comuna_ciudadano'] = df['comuna_ciudadano'].astype(str).apply(lambda x: x.replace('.0', ''))
     df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'], format='%d/%m/%Y', errors='coerce').fillna(
-        pd.to_datetime(df['fecha_de_beneficio'], format='%Y/%m/%d', errors='coerce')
-    )
+            pd.to_datetime(df['fecha_de_beneficio'], format='%Y/%m/%d', errors='coerce')
+        )
+    df['fecha_de_beneficio'] = df['fecha_de_beneficio'].dt.strftime('%d-%m-%Y')
     df['monto_del_credito'] = df['monto_del_credito'].apply(lambda x: x.replace('$', '').strip().replace(',', '').replace('.00', '') if type(x) == str else x)
-    df['línea_credito'] = df['línea_credito'].apply(lambda x: x.lower().replace('_', ' ').replace('-', ' ') if type(x) == str else x)
+    df['línea_credito'] = df['línea_credito'].apply(lambda x: x.lower().replace('_', ' ').replace('-', ' ').strip() if type(x) == str else x)
+    df['sexo'].value_counts().to_list()
+    df = df.drop_duplicates()
     return df
